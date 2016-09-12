709045020d29c44c299fa8b0383c9b9434e65970
pedromb <pedromb2004@hotmail.com>
2016-09-12 00:23:29 -0300

    changed api route

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_api import views
 
 urlpatterns = [
-    url(r'^searchmetadata/(?P<keywords>[\w,]+)/$', views.SearchMetadata.as_view()),
-    url(r'^getdata/(?P<tableId>[\w,]+)/(?P<limit>[0-9]+)/$', views.GetData.as_view()),
+    url(r'^api/searchmetadata/(?P<keywords>[\w,]+)/$', views.SearchMetadata.as_view()),
+    url(r'^api/getdata/(?P<tableId>[\w,]+)/(?P<limit>[0-9]+)/$', views.GetData.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
